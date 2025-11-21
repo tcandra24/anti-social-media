7c7d8bfde71ebb9fe09270946d07c346848ae9de
Tito Candra <34393188+tcandra24@users.noreply.github.com>
2025-11-22 06:14:44 +0700

    Update

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -1,1 +1,23 @@
 "use server";
+
+import { createClient } from "@/lib/supabase/client";
+
+export const login = async (payload: { email: string; password: string }) => {
+  try {
+    const { auth } = createClient();
+    const { data, error } = await auth.signUp({
+      email: payload.email,
+      password: payload.password,
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error: unknown) {
+    return error;
+  }
+};
+
+export const register = (payload: { name: string; email: string; password: string }) => {};
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,12 +1,15 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
 
-  const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!, {
+  const supabase = createServerClient(supabaseUrl!, supabaseKey!, {
     cookies: {
       getAll() {
         return request.cookies.getAll();
@@ -30,21 +33,10 @@ export async function updateSession(request: NextRequest) {
 
   const user = data?.claims;
 
-  const publicPath = ["/auth/sign-in", "/auth/sign-up"];
-
-  // if (publicPath.some((path) => path === url.pathname)) {
-  //   if(user){
-  //     url.pathname = "/";
-  //     return NextResponse.redirect(url);
-  //   }
-
-  //    return NextResponse.next();
-  // }
-
-  const url = request.nextUrl.clone();
-  if (user && publicPath.some((path) => path === url.pathname)) {
+  if (!user && !request.nextUrl.pathname.startsWith("/auth")) {
     // no user, potentially respond by redirecting the user to the login page
-    url.pathname = "/";
+    const url = request.nextUrl.clone();
+    url.pathname = "/auth/login";
     return NextResponse.redirect(url);
   }
 
